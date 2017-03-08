865a9947a4c4620354b8f205900bd888fda3e312
Kaioru <keifuurawrs@gmail.com>
2017-03-08 22:15:19 +0800

    Change executor type

--- nautilus-server/src/main/java/co/kaioru/nautilus/server/IDaemon.java
+++ nautilus-server/src/main/java/co/kaioru/nautilus/server/IDaemon.java
@@ -1,6 +1,8 @@
 package co.kaioru.nautilus.server;
 
 import co.kaioru.nautilus.server.config.RemoteConfig;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -10,7 +12,7 @@ public interface IDaemon<CO extends RemoteConfig> extends Remote {
 
 	CO getConfig() throws RemoteException;
 
-	Executor getExecutor() throws RemoteException;
+	ListeningScheduledExecutorService getExecutor() throws RemoteException;
 
 	void ping(IDaemon<? extends CO> from) throws RemoteException;
 
--- nautilus-server/src/main/lombok/co/kaioru/nautilus/server/Daemon.java
+++ nautilus-server/src/main/lombok/co/kaioru/nautilus/server/Daemon.java
@@ -1,18 +1,19 @@
 package co.kaioru.nautilus.server;
 
 import co.kaioru.nautilus.server.config.RemoteConfig;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @Getter
 @Setter
 public abstract class Daemon<CO extends RemoteConfig> implements IDaemon<CO> {
 
-	private final Executor executor =
+	private final ListeningScheduledExecutorService executor =
 		MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(32));
 	private final CO config;
 
