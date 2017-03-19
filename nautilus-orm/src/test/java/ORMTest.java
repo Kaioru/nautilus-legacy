b4b8551a34c2735a8f411ac0733eb6e4d7bec715
Kaioru <keifuurawrs@gmail.com>
2017-03-19 15:29:35 +0800

    Move database stuff to a different properties

--- nautilus-orm/src/test/java/ORMTest.java
+++ nautilus-orm/src/test/java/ORMTest.java
@@ -5,13 +5,22 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Properties;
+
 import static org.junit.Assert.assertEquals;
 
 public class ORMTest {
 
 	@Test
-	public void test() {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("co.kaioru.nautilus.orm.jpa-test");
+	public void test() throws IOException {
+		Properties properties = new Properties();
+
+		properties.load(getClass().getResourceAsStream("database.properties"));
+
+		EntityManagerFactory entityManagerFactory = Persistence
+			.createEntityManagerFactory("co.kaioru.nautilus.orm.jpa", properties);
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 		Account account = new Account();
