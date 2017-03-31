e402a914ef0283a85e714126c8e70ecb2cf1465a
Kaioru <keifuurawrs@gmail.com>
2017-03-31 17:44:08 +0800

    Add creation and update timestamps to model

--- nautilus-orm/src/main/lombok/co/kaioru/nautilus/orm/Model.java
+++ nautilus-orm/src/main/lombok/co/kaioru/nautilus/orm/Model.java
@@ -1,12 +1,16 @@
 package co.kaioru.nautilus.orm;
 
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @MappedSuperclass
@@ -16,5 +20,13 @@ public abstract class Model implements IModel, Serializable {
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private int id;
+
+	@CreationTimestamp
+	@Column
+	private Date createdOn;
+
+	@UpdateTimestamp
+	@Column
+	private Date updatedOn;
 
 }
