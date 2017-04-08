6847cee52df7bade7e066169bd46bb9c258bb106
Kaioru <keifuurawrs@gmail.com>
2017-04-08 19:21:13 +0800

    Add setting field to return map when disconnect

--- nautilus-server/src/main/lombok/co/kaioru/nautilus/server/game/user/RemoteUserFactory.java
+++ nautilus-server/src/main/lombok/co/kaioru/nautilus/server/game/user/RemoteUserFactory.java
@@ -1,20 +1,21 @@
 package co.kaioru.nautilus.server.game.user;
 
+import co.kaioru.nautilus.core.field.IFieldObject;
+import co.kaioru.nautilus.core.field.template.FieldPortalTemplate;
+import co.kaioru.nautilus.core.field.template.FieldTemplate;
 import co.kaioru.nautilus.orm.account.Account;
 import co.kaioru.nautilus.orm.account.AccountState;
 import co.kaioru.nautilus.orm.account.Character;
 import co.kaioru.nautilus.server.IServer;
-import co.kaioru.nautilus.server.config.ServerConfig;
 import co.kaioru.nautilus.server.migration.IServerMigration;
-import co.kaioru.nautilus.server.packet.game.SocketStructures;
 import io.netty.channel.Channel;
 
 import javax.persistence.EntityManager;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.sql.Date;
 import java.time.Instant;
+import java.util.Comparator;
 
 public class RemoteUserFactory implements IRemoteUserFactory {
 
@@ -71,7 +72,24 @@ public class RemoteUserFactory implements IRemoteUserFactory {
 
 			@Override
 			public void close() {
+				Character character = getCharacter();
+				IFieldObject fieldObject = getFieldObject();
 				Account account = getAccount();
+
+				if (character != null) {
+					if (fieldObject != null) {
+						// TODO: Spawn point
+						FieldTemplate fieldTemplate = fieldObject.getFieldInstance().getTemplate();
+						int fieldId = fieldTemplate.getReturnMap();
+						int spawnPoint = 0;
+
+						if (fieldId == 999999999)
+							fieldId = fieldTemplate.getTemplateID();
+
+						character.setFieldId(fieldId);
+						character.setSpawnPoint(spawnPoint);
+					}
+				}
 
 				if (account != null) {
 					if (account.getState() != AccountState.MIGRATING)
