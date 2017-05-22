2945c24851123dc51c0f4ed0b59b64a056692774
Kaioru <keifuurawrs@gmail.com>
2017-05-22 10:01:59 +0800

    Start recode again

--- nautilus-server/src/main/kotlin/co/kaioru/nautilus/sharding/ICluster.kt
+++
@@ -1,9 +0,0 @@
-package co.kaioru.nautilus.sharding
-
-import java.rmi.RemoteException
-
-interface ICluster : IStub {
-	val shards: MutableSet<IShard<*>>
-		@Throws(RemoteException::class)
-		get
-}
--- nautilus-server/src/main/kotlin/co/kaioru/nautilus/sharding/IShard.kt
+++
@@ -1,9 +0,0 @@
-package co.kaioru.nautilus.sharding
-
-import java.rmi.RemoteException
-
-interface IShard<C : ICluster> : IStub {
-	val clusters: MutableSet<C>
-		@Throws(RemoteException::class)
-		get
-}
